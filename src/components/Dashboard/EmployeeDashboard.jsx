7a1b06650d4a7d8afd8af520d801f13665f13e4d
ronakdadwani <dadwanironak456@gmail.com>
2025-01-13 23:36:41 +0530

    employ dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Header from '../other/header'
+
 
 const EmployeeDashboard = () => {
   return (
     <div>
-      <div>
-        
-      </div>
+      <header/>
     </div>
   )
 }
