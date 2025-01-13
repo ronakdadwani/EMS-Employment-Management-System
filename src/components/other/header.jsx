3cec8348915108097c0bcfc849f20264a22c33e2
ronakdadwani <dadwanironak456@gmail.com>
2025-01-14 00:42:08 +0530

    header.jsx heading

--- src/components/other/header.jsx
+++ src/components/other/header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between items-center p-4 bg-gray-800 text-white'>
-      <h1>Hello <br />Ronak 👋</h1>
-      <button>Log Out</button>
+    <div className='flex justify-between items-end'>
+      <h1 className='text-2xl font-medium '>Hello <br /> <span className='text-3xl font-semibold'>Ronak 👋 </span> </h1>
+      <button className='bg-red-500 text-lg font-medium text-white px-5 py-2 rounded-sm '>Log Out</button>
     </div>
   )
 }
