01f8013127f7c333539177c3d2b1e97695abaf40
ronakdadwani <dadwanironak456@gmail.com>
2025-01-14 00:42:32 +0530

    task list component

---
+++ src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskListNUmbers = () => {
+  return (
+    <div className='flex justify-between gap-5'>
+        <div className='h-40 w-1/2 bg-red-500'></div>
+    </div>
+  )
+}
+
+export default TaskListNUmbers
