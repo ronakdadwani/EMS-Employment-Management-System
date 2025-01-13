26fb19e27d7617de941fced084c310284c403095
ronakdadwani <dadwanironak456@gmail.com>
2025-01-14 00:41:54 +0530

    employe

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../other/header'
+import TaskListNUmbers from '../other/TaskListNUmbers'
 
 
 const EmployeeDashboard = () => {
   return (
-    <div>
-      <header/>
+    <div className='p-10 bg-[#1C1C1C] h-screen '> 
+      <Header/>
+      <TaskListNUmbers/>
     </div>
   )
 }
