bb0eb1f17984df2340fdd9cdda042f105a81bb75
ronakdadwani <dadwanironak456@gmail.com>
2025-01-14 23:41:01 +0530

    task list component

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div className='h-[55%] w-full py-5 bg-red-400 mt-10 flex items-center justify-flex-start gap-5'>
+        <div className='h-full w-[300px] bg-blue-400 rounded-xl'>
+
+        </div>
+        <div className='h-full w-[300px] bg-blue-400 rounded-xl'>
+
+        </div>
+        <div className='h-full w-[300px] bg-blue-400 rounded-xl'>
+
+        </div>
+        <div className='h-full w-[300px] bg-blue-400 rounded-xl'>
+
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
