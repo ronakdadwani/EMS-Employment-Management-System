f0c166e3b6fef576c63fe4e3ae2be806c48082d6
ronakdadwani <dadwanironak456@gmail.com>
2025-01-14 23:41:13 +0530

    task list number component

--- src/components/other/TaskListNumbers.jsx
+++ src/components/other/TaskListNumbers.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const TaskListNUmbers = () => {
   return (
-    <div className='flex justify-between gap-5'>
-        <div className='h-40 w-1/2 bg-red-500'></div>
+    <div className='flex mt-10 justify-between gap-5 screen'>
+        <div className='rounded-xl px-9 py-6 w-[45%] bg-red-400'>
+          <h2 className='text-3xl font-semibold'>0</h2>
+          <h3 className='text-xl font-medium'>New Task</h3>
+        </div>
+        <div className='rounded-xl px-9 py-6 w-[45%] bg-blue-400'>
+          <h2 className='text-3xl font-semibold'>0</h2>
+          <h3 className='text-xl font-medium'>New Task</h3>
+        </div>
+        <div className='rounded-xl px-9 py-6 w-[45%] bg-green-400'>
+          <h2 className='text-3xl font-semibold'>0</h2>
+          <h3 className='text-xl font-medium'>New Task</h3>
+        </div>
+        <div className='rounded-xl px-9 py-6 w-[45%] bg-yellow-400'>
+          <h2 className='text-3xl font-semibold'>0</h2>
+          <h3 className='text-xl font-medium'>New Task</h3>
+        </div>
     </div>
   )
 }
